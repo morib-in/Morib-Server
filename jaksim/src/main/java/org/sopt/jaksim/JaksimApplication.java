ab3f02ffb3e06172453d58bef6e597106231a931
geniuus <cross_man@naver.com>
2024-06-16 18:08:27 +0900

    Init 프로젝트 생성
    Create Spring Boot Project

---
+++ jaksim/src/main/java/org/sopt/jaksim/JaksimApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.jaksim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaksimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaksimApplication.class, args);
+    }
+
+}
---
+++ jaksim/src/test/java/org/sopt/jaksim/JaksimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.jaksim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaksimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
