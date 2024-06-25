2b8f56a2b40435430bfb6668f29683e13a291082
geniuus <cross_man@naver.com>
2024-06-25 16:32:07 +0900

    Feat [#13] Facade 패턴 적용

--- jaksim/src/main/java/org/sopt/jaksim/user/facade/UserFacade.java
+++ jaksim/src/main/java/org/sopt/jaksim/user/facade/UserFacade.java
@@ -1,4 +1,17 @@
 package org.sopt.jaksim.user.facade;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.jaksim.user.dto.request.UserReissueRequest;
+import org.sopt.jaksim.user.dto.response.UserSignInResponse;
+import org.sopt.jaksim.user.service.UserService;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class UserFacade {
+    private final UserService userService;
+
+    public UserSignInResponse reissue(String refreshToken, UserReissueRequest userReissueRequest) {
+        return userService.reissue(refreshToken, userReissueRequest);
+    }
 }
