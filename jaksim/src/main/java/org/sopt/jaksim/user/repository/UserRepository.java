0c34b082ad1814fde985e9ccaf13e825ef7ca505
geniuus <cross_man@naver.com>
2024-06-25 16:31:53 +0900

    Fix [#13] Reissue API 수정

--- jaksim/src/main/java/org/sopt/jaksim/user/repository/UserRepository.java
+++ jaksim/src/main/java/org/sopt/jaksim/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package org.sopt.jaksim.user.repository;
 
+import org.sopt.jaksim.user.domain.Platform;
 import org.sopt.jaksim.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByPlatformAndEmail(Platform platform, String email);
 }
