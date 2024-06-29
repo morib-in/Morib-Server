85831951c93662dcba244f3a4399c352abce82fc
geniuus <cross_man@naver.com>
2024-06-29 18:42:08 +0900

    Feat [#16] 로그인, 회원가입 컨트롤러 구현

--- jaksim/src/main/java/org/sopt/jaksim/user/api/UserApiController.java
+++ jaksim/src/main/java/org/sopt/jaksim/user/api/UserApiController.java
@@ -1,36 +1,42 @@
 package org.sopt.jaksim.user.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.sopt.jaksim.auth.PrincipalHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.sopt.jaksim.global.common.ApiResponseUtil;
 import org.sopt.jaksim.global.common.BaseResponse;
 import org.sopt.jaksim.global.message.SuccessMessage;
+import org.sopt.jaksim.user.dto.request.UserSignUpRequest;
 import org.sopt.jaksim.user.dto.request.UserReissueRequest;
 import org.sopt.jaksim.user.dto.request.UserSignInRequest;
 import org.sopt.jaksim.user.dto.response.UserSignInResponse;
+import org.sopt.jaksim.user.dto.response.UserSignUpResponse;
 import org.sopt.jaksim.user.facade.UserFacade;
-import org.sopt.jaksim.user.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import static org.sopt.jaksim.global.common.Constants.AUTHORIZATION;
-import static org.sopt.jaksim.global.message.SuccessMessage.USER_SIGN_IN_SUCCESS;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class UserApiController {
+public class UserApiController implements UserApi{
 
     private final UserFacade userFacade;
 
-//    @PostMapping("/user/login")
-//    public ResponseEntity<BaseResponse<UserSignInResponse>> login(@RequestBody UserSignInRequest userSignInRequest) {
-//        final UserSignInResponse response = userFacade.login(userSignInRequest);
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .body(SuccessStatusResponse.of(SuccessMessage.USER_SIGN_IN_SUCCESS, response));
-//    }
+    @PostMapping("/auth/google/callback")
+    public ResponseEntity<BaseResponse<?>> signup(@RequestBody final UserSignUpRequest userSignUpRequest) {
+        UserSignUpResponse response = userFacade.signup(userSignUpRequest);
+        return ApiResponseUtil.success(SuccessMessage.USER_SIGN_UP_SUCCESS, response);
+    }
+
+    @Override
+    @PostMapping("/user/signin")
+    public ResponseEntity<BaseResponse<?>> signin(@RequestHeader(AUTHORIZATION) final String accessToken) {
+        final UserSignInResponse response = userFacade.signin();
+        return ApiResponseUtil.success(SuccessMessage.USER_SIGN_IN_SUCCESS, response);
+    }
 //
 
     @PostMapping("/reissue")
