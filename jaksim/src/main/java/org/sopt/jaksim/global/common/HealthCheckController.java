26688a58b68d0747b98ab5de3f9817116365ee69
geniuus <cross_man@naver.com>
2024-06-29 18:40:20 +0900

    Chore [#16] 자잘한 import 코드 추가

--- jaksim/src/main/java/org/sopt/jaksim/global/common/HealthCheckController.java
+++ jaksim/src/main/java/org/sopt/jaksim/global/common/HealthCheckController.java
@@ -2,6 +2,8 @@ package org.sopt.jaksim.global.common;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.sopt.jaksim.auth.UserAuthentication;
+import org.sopt.jaksim.auth.jwt.JwtTokenProvider;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
