b6cad90f8d0de8d692e47fab844ad7ce959e5e11
geniuus <cross_man@naver.com>
2024-06-29 18:43:16 +0900

    Fix [#16] facade 수정
    서비스의 여러 함수를 호출해서 메소드를 만드는 방식이 아니라, 서비스의 한 함수에서 여러 메소드를 호출하는 방식

--- jaksim/src/main/java/org/sopt/jaksim/user/facade/UserFacade.java
+++ jaksim/src/main/java/org/sopt/jaksim/user/facade/UserFacade.java
@@ -1,8 +1,13 @@
 package org.sopt.jaksim.user.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.jaksim.user.dto.Tokens;
+import org.sopt.jaksim.user.dto.UserInfo;
+import org.sopt.jaksim.user.dto.request.UserSignUpRequest;
 import org.sopt.jaksim.user.dto.request.UserReissueRequest;
+import org.sopt.jaksim.user.dto.request.UserSignInRequest;
 import org.sopt.jaksim.user.dto.response.UserSignInResponse;
+import org.sopt.jaksim.user.dto.response.UserSignUpResponse;
 import org.sopt.jaksim.user.service.UserService;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +16,18 @@ import org.springframework.stereotype.Component;
 public class UserFacade {
     private final UserService userService;
 
+    public UserSignUpResponse signup(UserSignUpRequest userSignUpRequest) {
+        return userService.signup(userSignUpRequest);
+    }
+
+    public UserSignInResponse signin() {
+        return userService.signIn();
+    }
+
     public UserSignInResponse reissue(String refreshToken, UserReissueRequest userReissueRequest) {
         return userService.reissue(refreshToken, userReissueRequest);
     }
+
+
+
 }
