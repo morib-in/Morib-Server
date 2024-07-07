f056b15365033b6e9ca7d363b2178bbbf5c9b80c
geniuus <cross_man@naver.com>
2024-07-08 02:47:09 +0900

    Fix [#26] Success Status Code 수정
    200 OK로 통일

--- jaksim/src/main/java/org/sopt/jaksim/user/api/UserApiController.java
+++ jaksim/src/main/java/org/sopt/jaksim/user/api/UserApiController.java
@@ -3,6 +3,8 @@ package org.sopt.jaksim.user.api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.sopt.jaksim.global.common.ApiResponseUtil;
 import org.sopt.jaksim.global.common.BaseResponse;
 import org.sopt.jaksim.global.message.SuccessMessage;
@@ -14,6 +16,8 @@ import org.sopt.jaksim.user.dto.response.UserSignUpResponse;
 import org.sopt.jaksim.user.facade.UserFacade;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 import static org.sopt.jaksim.global.common.Constants.AUTHORIZATION;
 
@@ -28,23 +32,22 @@ public class UserApiController implements UserApi{
     @PostMapping("/auth/google/callback")
     public ResponseEntity<BaseResponse<?>> signup(@RequestBody final UserSignUpRequest userSignUpRequest) {
         UserSignUpResponse response = userFacade.signup(userSignUpRequest);
-        return ApiResponseUtil.success(SuccessMessage.USER_SIGN_UP_SUCCESS, response);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
     }
 
     @Override
     @PostMapping("/users/signin")
     public ResponseEntity<BaseResponse<?>> signin(@RequestHeader(AUTHORIZATION) final String accessToken) {
         final UserSignInResponse response = userFacade.signin();
-        return ApiResponseUtil.success(SuccessMessage.USER_SIGN_IN_SUCCESS, response);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
     }
 
     @PostMapping("/users/reissue")
     public ResponseEntity<BaseResponse<?>> reissue(@RequestHeader(AUTHORIZATION) final String refreshToken,
                                                       @RequestBody final UserReissueRequest userReissueRequest) {
         UserSignInResponse response = userFacade.reissue(refreshToken, userReissueRequest);
-        return ApiResponseUtil.success(SuccessMessage.USER_TOKEN_REISSUE_SUCCESS, response);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
     }
-
 }
 
 
