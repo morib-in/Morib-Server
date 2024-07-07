bd5346ba4a379fcfc6ea5463cb6fd39be8bad53d
geniuus <cross_man@naver.com>
2024-07-08 02:04:25 +0900

    Feat [#26] ErrorMessage 추가

--- jaksim/src/main/java/org/sopt/jaksim/global/message/ErrorMessage.java
+++ jaksim/src/main/java/org/sopt/jaksim/global/message/ErrorMessage.java
@@ -14,6 +14,9 @@ public enum ErrorMessage {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "e4000", "잘못된 요청입니다."),
     INVALID_PLATFORM_TYPE(HttpStatus.BAD_REQUEST, "e4001", "유효하지 않은 플랫폼 타입입니다."),
     INVALID_GRANT_BY_OAUTH(HttpStatus.BAD_REQUEST, "e4002", "유효하지 않은 인가 코드입니다. 이미 회원가입된 사용자입니다."),
+    INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST, "e4003", "유효하지 않은 날짜 형식입니다. yyyyMMdd 형식으로 작성해주세요."),
+    IS_NOT_TODAY(HttpStatus.BAD_REQUEST, "e4004", "targetDate가 오늘 날짜가 아닙니다."),
+    INVALID_URL(HttpStatus.BAD_REQUEST, "e4005", "요청된 url이 유효하지 않습니다."),
     /**
      * 401 Unauthorized
      */
