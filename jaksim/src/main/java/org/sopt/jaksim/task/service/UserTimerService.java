7e6d9b154c38b594dc3d167a2dda3cc8989ea590
geniuus <cross_man@naver.com>
2024-07-08 02:47:32 +0900

    Feat [#26] UserTimerService 구현

---
+++ jaksim/src/main/java/org/sopt/jaksim/task/service/UserTimerService.java
@@ -0,0 +1,54 @@
+package org.sopt.jaksim.task.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.jaksim.global.common.DateUtil;
+import org.sopt.jaksim.global.exception.InvalidValueException;
+import org.sopt.jaksim.global.message.ErrorMessage;
+import org.sopt.jaksim.task.domain.UserTimer;
+import org.sopt.jaksim.task.dto.StopTimerRequest;
+import org.sopt.jaksim.task.dto.TotalTimeTodayResponse;
+import org.sopt.jaksim.task.repository.UserTimerRepository;
+import org.sopt.jaksim.user.domain.User;
+import org.sopt.jaksim.user.facade.UserFacade;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserTimerService {
+    private final UserTimerRepository userTimerRepository;
+    private final UserFacade userFacade;
+
+    public TotalTimeTodayResponse getTotalTimeToday(String targetDate) {
+        if (!isToday(targetDate)) {
+            throw new InvalidValueException(ErrorMessage.IS_NOT_TODAY);
+        }
+        User user = userFacade.getUserByPrincipal();
+        UserTimer userTimer = getUserTimerByUserIdAndTargetDate(user.getId(), targetDate);
+        if (userTimer == null) {
+            userTimer = userTimerRepository.save(UserTimer.create(user.getId(), targetDate));
+        }
+        return TotalTimeTodayResponse.of(targetDate, userTimer.getTargetTime());
+    }
+
+    public boolean isToday(String targetDate) {
+        if (!LocalDate.now().equals(DateUtil.stringToDate(targetDate))) {
+            return false;
+        }
+        return true;
+    }
+
+    public UserTimer getUserTimerByUserIdAndTargetDate(Long userId, String targetDate) {
+        return userTimerRepository.findByUserIdAndTargetDate(userId, targetDate);
+    }
+
+    public void calculateUserTimerOnStop(StopTimerRequest stopTimerRequest) {
+        User user = userFacade.getUserByPrincipal();
+        UserTimer userTimer = userTimerRepository.findByUserId(user.getId());
+        userTimer.setTargetTime(userTimer.getTargetTime() + stopTimerRequest.elapsedTime());
+    }
+
+}
