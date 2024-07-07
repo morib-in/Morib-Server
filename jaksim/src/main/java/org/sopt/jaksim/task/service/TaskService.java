07fe1fba1f94f15a828dcce51a7e2d67a5c5bdfd
geniuus <cross_man@naver.com>
2024-07-08 02:47:49 +0900

    Feat [#26] TaskService 생성만

---
+++ jaksim/src/main/java/org/sopt/jaksim/task/service/TaskService.java
@@ -0,0 +1,14 @@
+package org.sopt.jaksim.task.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+}
