585692c2750b0d50e6834730fc629a256168b8b8
geniuus <cross_man@naver.com>
2024-07-08 02:03:45 +0900

    Feat [#26] UserTimer 엔티티 클래스

--- jaksim/src/main/java/org/sopt/jaksim/task/domain/UserTimer.java
+++ jaksim/src/main/java/org/sopt/jaksim/task/domain/UserTimer.java
@@ -10,12 +10,21 @@ import java.time.LocalDate;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
+@Setter
 @RedisHash(value = "user_timer", timeToLive = 60 * 60 * 24 * 1000L)
 public class UserTimer {
     @Id
     private Long id;
-    @Indexed
     private Long userId;
+    @Indexed
     private String targetDate;
     private int targetTime;
+
+    public static UserTimer create(Long userId, String targetDate) {
+        return UserTimer.builder()
+                .userId(userId)
+                .targetDate(targetDate)
+                .targetTime(0)
+                .build();
+    }
 }
