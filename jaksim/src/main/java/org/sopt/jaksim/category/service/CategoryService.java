be38b25430bfe6f60cdcdc19ff13294b226a394f
geniuus <cross_man@naver.com>
2024-07-09 16:51:02 +0900

    Feat [#20] category 조회 service

--- jaksim/src/main/java/org/sopt/jaksim/category/service/CategoryService.java
+++ jaksim/src/main/java/org/sopt/jaksim/category/service/CategoryService.java
@@ -3,11 +3,23 @@ package org.sopt.jaksim.category.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.sopt.jaksim.category.domain.Category;
+import org.sopt.jaksim.category.domain.CategoryTask;
 import org.sopt.jaksim.category.dto.CategoryCreateRequest;
+import org.sopt.jaksim.category.dto.CategoryTaskLink;
 import org.sopt.jaksim.category.repository.CategoryRepository;
+import org.sopt.jaksim.category.repository.CategoryTaskRepository;
+import org.sopt.jaksim.global.common.DateUtil;
 import org.sopt.jaksim.mset.service.MsetService;
+
+import org.sopt.jaksim.task.domain.Task;
+import org.sopt.jaksim.task.repository.TaskRepository;
+import org.sopt.jaksim.task.service.TaskService;
 import org.sopt.jaksim.user.facade.UserFacade;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -15,16 +27,47 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final CategoryTaskRepository categoryTaskRepository;
+    private final TaskRepository taskRepository;
     private final UserFacade userFacade;
     private final MsetService msetService;
+    private final TaskService taskService;
 
     public void create(CategoryCreateRequest categoryCreateRequest) {
         Category category = Category.create(
                 categoryCreateRequest.name(),
                 userFacade.getUserByPrincipal().getId(),
-                categoryCreateRequest.startDate(),
-                categoryCreateRequest.endDate());
+                DateUtil.stringToDate(categoryCreateRequest.startDate()),
+                DateUtil.stringToDate(categoryCreateRequest.endDate()));
         category = categoryRepository.save(category);
         msetService.createByCategory(categoryCreateRequest, category.getId());
+    }
+
+    // 유저의 카테고리를 구간에 맞게 가져옴
+    public List<Category> getCategoriesInRange(Long userId, LocalDate startDate, LocalDate endDate) {
+        return categoryRepository.findByUserIdWithRange(userId, startDate, endDate);
+    }
+
+    public List<CategoryTaskLink> getCategoryTaskByCategories(List<Category> categoryList) {
+        List<CategoryTaskLink> categoryTaskLinkList = new ArrayList<>();
+        for (Category category : categoryList) {
+            // category에 대한 categoryTask 조회
+            List<CategoryTask> categoryTaskList = categoryTaskRepository.findByCategoryId(category.getId());
+            List<Task> taskList = new ArrayList<>();
+            // 해당 category에 연결된 categoryTask들의 task들을 조회해서 categoryTaskLinkList에 주입
+            for (CategoryTask categoryTask : categoryTaskList) {
+                // 해당 categoryTask의 taskId로 task를 조회해서 taskList에 주입
+                taskList.add(taskService.getTaskById(categoryTask.getTaskId()));
+            }
+            categoryTaskLinkList.add(CategoryTaskLink.of(category,taskList));
+        }
+        return categoryTaskLinkList;
+    }
+
+    public boolean isContains(Category category, LocalDate idxDate) {
+        return (category.getStartDate().isBefore(idxDate) &&
+                category.getEndDate().isAfter(idxDate)) ||
+                category.getStartDate().equals(idxDate) ||
+                category.getEndDate().equals(idxDate);
     }
 }
