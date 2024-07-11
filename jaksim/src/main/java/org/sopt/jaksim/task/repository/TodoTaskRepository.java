fae9ba5da45e467e9e667e6549f7b5e4422e7c48
geniuus <cross_man@naver.com>
2024-07-11 23:29:24 +0900

    Feat [#39] TodoTask Repository

---
+++ jaksim/src/main/java/org/sopt/jaksim/task/repository/TodoTaskRepository.java
@@ -0,0 +1,11 @@
+package org.sopt.jaksim.task.repository;
+
+import org.sopt.jaksim.task.domain.TodoTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TodoTaskRepository extends JpaRepository<TodoTask, Long> {
+    List<TodoTask> findByTodoId(Long todoId);
+}
