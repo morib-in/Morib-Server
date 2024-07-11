574c30b3a4c0347f56264b8dcd3ae3200741653c
geniuus <cross_man@naver.com>
2024-07-11 23:31:26 +0900

    Feat [#39] CategoryMsetRepository

--- jaksim/src/main/java/org/sopt/jaksim/mset/repository/CategoryMsetRepository.java
+++ jaksim/src/main/java/org/sopt/jaksim/mset/repository/CategoryMsetRepository.java
@@ -3,5 +3,8 @@ package org.sopt.jaksim.mset.repository;
 import org.sopt.jaksim.mset.domain.CategoryMset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryMsetRepository extends JpaRepository<CategoryMset, Long> {
+    List<CategoryMset> findByCategoryId(Long categoryId);
 }
