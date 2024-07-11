6a1c19485c08a7415b88e37a79719bcd0269e27e
geniuus <cross_man@naver.com>
2024-07-11 23:33:08 +0900

    Feat [#39] UserTimerRepository

--- jaksim/src/main/java/org/sopt/jaksim/task/repository/UserTimerRepository.java
+++ jaksim/src/main/java/org/sopt/jaksim/task/repository/UserTimerRepository.java
@@ -1,11 +1,12 @@
 package org.sopt.jaksim.task.repository;
 
 import org.sopt.jaksim.task.domain.UserTimer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserTimerRepository extends CrudRepository<UserTimer, Long> {
+public interface UserTimerRepository extends JpaRepository<UserTimer, Long> {
     UserTimer findByUserIdAndTargetDate(Long userId, String targetDate);
     UserTimer findByUserId(Long userId);
 }
