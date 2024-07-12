6c7b3020ac26f2b5d48043c399437f86ffe8ea31
geniuus <cross_man@naver.com>
2024-07-12 20:25:12 +0900

    Feat [#42] TaskTimer 생성로직 구현

--- jaksim/src/main/java/org/sopt/jaksim/task/domain/TaskTimer.java
+++ jaksim/src/main/java/org/sopt/jaksim/task/domain/TaskTimer.java
@@ -4,6 +4,7 @@ package org.sopt.jaksim.task.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.sopt.jaksim.global.common.BaseTimeEntity;
+import org.sopt.jaksim.task.repository.TaskTimerRepository;
 
 import java.time.LocalDate;
 
@@ -25,4 +26,12 @@ public class TaskTimer extends BaseTimeEntity {
     private LocalDate targetDate;
     private int targetTime;
 
+    public static TaskTimer create(Long userId, Long taskId) {
+        return TaskTimer.builder()
+                .userId(userId)
+                .taskId(taskId)
+                .targetDate(LocalDate.now())
+                .targetTime(0)
+                .build();
+    }
 }
