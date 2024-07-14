25ff9798857b8e7a1ca01c0d48f3aea6067ff1fe
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-07-14 15:37:18 +0900

    Fix [#45] Mset 관련 api 스웨거적용

--- jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
+++ jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
@@ -3,6 +3,7 @@ package org.sopt.jaksim.mset.api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
+import org.sopt.jaksim.category.api.CategoryApi;
 import org.sopt.jaksim.category.dto.CategoryMsetLinkResponse;
 import org.sopt.jaksim.category.facade.CategoryMsetFacade;
 import org.sopt.jaksim.global.common.ApiResponseUtil;
@@ -18,10 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class MsetApiController {
+public class MsetApiController implements MsetApi {
     private final CategoryMsetFacade categoryMsetFacade;
 
     @GetMapping("/mset/categories/{categoryId}")
+    @Override
     public ResponseEntity<BaseResponse<?>> getFromOtherCategory(@PathVariable("categoryId") Long categoryId) {
         CategoryMsetLinkResponse response = categoryMsetFacade.getFromOtherCategory(categoryId);
         return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
