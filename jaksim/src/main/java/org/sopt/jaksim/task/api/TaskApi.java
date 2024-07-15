89e7f43d4cdf85a664e508acadde8dd669287067
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-07-16 00:26:17 +0900

    Fix [#93] TaskApi 오류 수정

--- jaksim/src/main/java/org/sopt/jaksim/task/api/TaskApi.java
+++ jaksim/src/main/java/org/sopt/jaksim/task/api/TaskApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.sopt.jaksim.global.common.BaseResponse;
+import org.sopt.jaksim.task.dto.FetchTitleResponse;
 import org.sopt.jaksim.task.dto.TaskCreateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +20,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface TaskApi {
     @Operation(
             summary = "URL로 탭 이름 가져오기 API",
+            description = "모립세트의 url로 브라우저 탭 이름을 가져오는 API입니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
                             description = "요청이 성공했습니다.",
-                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+                            content = @Content(schema = @Schema(implementation = FetchTitleResponse.class))),
                     @ApiResponse(
                             responseCode = "400",
                             description = "잘못된 요청입니다.",
@@ -47,14 +49,16 @@ public interface TaskApi {
                     @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류입니다.")})
-    public ResponseEntity<BaseResponse<?>> fetchTitle(@RequestParam("requestUrl") String requestUrl);
+    ResponseEntity<BaseResponse<?>> fetchTitle(@RequestParam("requestUrl") String requestUrl);
 
     @Operation(
             summary = "태스크 생성 API",
+            description = "사용자가 \"태스크 추가\" 버튼을 클릭할 때 호출하는 API입니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "요청이 성공했습니다."),
+                            description = "요청이 성공했습니다.",
+                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
                     @ApiResponse(
                             responseCode = "400",
                             description = "잘못된 요청입니다.",
@@ -78,16 +82,18 @@ public interface TaskApi {
                     @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류입니다.")})
-    public ResponseEntity<BaseResponse<?>> create(@PathVariable("categoryId") Long categoryId,
+    ResponseEntity<BaseResponse<?>> create(@PathVariable("categoryId") Long categoryId,
                                                   @RequestBody TaskCreateRequest taskCreateRequest);
 
 
     @Operation(
             summary = "태스크 상태 변경 API",
+            description = "사용자가 \"할 일 완료 체크박스\"를 클릭하여 태스크의 완료 상태를 변경할 때 사용, 이 API를 보내면 원래의 isEnd 상태가 토글됩니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "요청이 성공했습니다."),
+                            description = "요청이 성공했습니다.",
+                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
                     @ApiResponse(
                             responseCode = "400",
                             description = "잘못된 요청입니다.",
@@ -103,5 +109,5 @@ public interface TaskApi {
                     @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류입니다.")})
-    public ResponseEntity<BaseResponse<?>> toggleTaskStatus(@PathVariable("taskId") Long taskId);
+    ResponseEntity<BaseResponse<?>> toggleTaskStatus(@PathVariable("taskId") Long taskId);
 }
