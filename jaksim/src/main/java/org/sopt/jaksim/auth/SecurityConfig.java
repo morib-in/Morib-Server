226a04afeb91dc792e14a11e9931620dfe4c7d04
geniuus <cross_man@naver.com>
2024-07-15 18:40:38 +0900

    Fix [#87] white-list 전부 열기

--- jaksim/src/main/java/org/sopt/jaksim/auth/SecurityConfig.java
+++ jaksim/src/main/java/org/sopt/jaksim/auth/SecurityConfig.java
@@ -13,14 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.RequestCac
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsUtils;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-import static org.sopt.jaksim.global.common.Constants.ACTIVATE_PROFILE_URL;
-
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity //web Security를 사용할 수 있게
@@ -30,10 +22,11 @@ public class SecurityConfig {
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     private static final String[] AUTH_WHITE_LIST = {
-            ACTIVATE_PROFILE_URL,
-             "/api/v1/users/signin/**", "/api/v1/auth/**",
-            "/api/v1/users/reissue/**", "/login/**",
-            "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
+//            ACTIVATE_PROFILE_URL,
+//             "/api/v1/users/signin/**", "/api/v1/auth/**",
+//            "/api/v1/users/reissue/**", "/login/**",
+//            "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**"
+            "/**"
     };
 
     @Bean
