c11bdce713c1d9f403ad0f45874a00cbf647755e
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-07-16 00:26:01 +0900

    Fix [#93] CategoryApi 오류 수정

--- jaksim/src/main/java/org/sopt/jaksim/category/api/CategoryApi.java
+++ jaksim/src/main/java/org/sopt/jaksim/category/api/CategoryApi.java
@@ -7,7 +7,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.jaksim.category.dto.CategoryCheckResponse;
 import org.sopt.jaksim.category.dto.CategoryCreateRequest;
+import org.sopt.jaksim.category.dto.FilteredResourceResponse;
 import org.sopt.jaksim.global.common.BaseResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +23,12 @@ public interface CategoryApi {
 
     @Operation(
             summary = "카테고리 생성 API",
+            description = "새로운 작업 카테고리를 생성하는 API입니다",
             responses = {
                     @ApiResponse(
-                            responseCode = "201",
-                            description = "요청이 성공했습니다."),
+                            responseCode = "200",
+                            description = "요청이 성공했습니다.",
+                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
 
                     @ApiResponse(
                             responseCode = "400",
@@ -47,17 +51,18 @@ public interface CategoryApi {
                     @ApiResponse(
                             responseCode = "500",
                             description = "서버 내부 오류입니다.")})
-    public ResponseEntity<BaseResponse<?>> create(@RequestBody CategoryCreateRequest request);
+    ResponseEntity<BaseResponse<?>> create(@RequestBody CategoryCreateRequest request);
 
 
 
     @Operation(
             summary = "전체 카테고리, 태스크 조희 API (홈뷰)",
+            description = "사용자의 전체 작업 카테고리, 그에 해당하는 태스크, 모립 세트까지 전부 조회하는 API(페이지네이션)입니다.",
             responses = {
                     @ApiResponse(
-                            responseCode = "201",
+                            responseCode = "200",
                             description = "요청이 성공했습니다.",
-                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+                            content = @Content(schema = @Schema(implementation = FilteredResourceResponse.class))),
 
                     @ApiResponse(
                             responseCode = "400",
@@ -88,17 +93,17 @@ public interface CategoryApi {
                             responseCode = "500",
                             description = "서버 내부 오류입니다.",
                             content = @Content)})
-
-    public ResponseEntity<BaseResponse<?>> retrieve(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+    ResponseEntity<BaseResponse<?>> retrieve(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
                                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate);
 
     @Operation(
             summary = "카테고리 조희 API",
+            description = "사용자가 생성한 모든 카테고리를 검색하는 API입니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
                             description = "요청이 성공했습니다.",
-                            content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+                            content = @Content(schema = @Schema(implementation = CategoryCheckResponse.class))),
                     @ApiResponse(
                             responseCode = "400",
                             description = "잘못된 요청입니다.",
