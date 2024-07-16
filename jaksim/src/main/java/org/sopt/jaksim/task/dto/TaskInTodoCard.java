f7e24a27740d62c3845faaf43c6596cdf5328831
geniuus <cross_man@naver.com>
2024-07-17 00:43:32 +0900

    Fix [#111] 할일카드에 필요 데이터 추가 후 정렬

--- jaksim/src/main/java/org/sopt/jaksim/task/dto/TaskInTodoCard.java
+++ jaksim/src/main/java/org/sopt/jaksim/task/dto/TaskInTodoCard.java
@@ -1,25 +1,33 @@
 package org.sopt.jaksim.task.dto;
 
+import org.sopt.jaksim.category.domain.Category;
 import org.sopt.jaksim.task.domain.Task;
 import org.sopt.jaksim.task.domain.TaskTimer;
 
 import java.time.LocalDate;
 
 public record TaskInTodoCard(
+        Long id,
+        String categoryName,
         String name,
         LocalDate startDate,
         LocalDate endDate,
         boolean isComplete,
         LocalDate targetDate,
-        int targetTime
+        int targetTime,
+        int taskOrder
 ) {
-    public static TaskInTodoCard of(Task task, TaskTimer taskTimer) {
+    public static TaskInTodoCard of(Task task, TaskTimer taskTimer, Category category, int taskOrder) {
         return new TaskInTodoCard(
+                task.getId(),
+                category.getName(),
                 task.getName(),
                 task.getStartDate(),
                 task.getEndDate(),
                 task.getIsComplete(),
                 taskTimer.getTargetDate(),
-                taskTimer.getTargetTime());
+                taskTimer.getTargetTime(),
+                taskOrder
+                );
     }
 }
