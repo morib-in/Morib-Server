7c195d039ad3eeb166b880d341f53265574353ad
geniuus <cross_man@naver.com>
2024-07-17 23:57:32 +0900

    Feat [#127] Controller 추가

--- jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
+++ jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
@@ -6,14 +6,18 @@ import org.apache.coyote.Response;
 import org.sopt.jaksim.category.api.CategoryApi;
 import org.sopt.jaksim.category.dto.CategoryMsetLinkResponse;
 import org.sopt.jaksim.category.facade.CategoryMsetFacade;
+import org.sopt.jaksim.category.facade.CategoryTaskFacade;
 import org.sopt.jaksim.global.common.ApiResponseUtil;
 import org.sopt.jaksim.global.common.BaseResponse;
 import org.sopt.jaksim.global.message.SuccessMessage;
+import org.sopt.jaksim.mset.dto.MsetOfTask;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class MsetApiController implements MsetApi {
     private final CategoryMsetFacade categoryMsetFacade;
+    private final CategoryTaskFacade categoryTaskFacade;
 
     @GetMapping("/mset/categories/{categoryId}")
     @Override
@@ -28,4 +33,11 @@ public class MsetApiController implements MsetApi {
         CategoryMsetLinkResponse response = categoryMsetFacade.getFromOtherCategory(categoryId);
         return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
     }
+
+    @GetMapping("/mset/tasks/{taskId}")
+    public ResponseEntity<BaseResponse<?>> getFromOtherTask(@PathVariable("taskId") Long taskId) {
+        List<MsetOfTask> response = categoryMsetFacade.getFromCategory(categoryTaskFacade.getCategoryIdByCategoryTask(taskId));
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
+    }
+
 }
