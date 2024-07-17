3e642ff342b366269f9e3ecfc71090ec5fd02f3e
geniuus <cross_man@naver.com>
2024-07-17 15:09:06 +0900

    Fix [#119] 헬스체크 API 수정

--- jaksim/src/main/java/org/sopt/jaksim/global/common/HealthCheckController.java
+++ jaksim/src/main/java/org/sopt/jaksim/global/common/HealthCheckController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.jaksim.auth.UserAuthentication;
 import org.sopt.jaksim.auth.jwt.JwtTokenProvider;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,7 @@ public class HealthCheckController {
     @GetMapping("/profile")
     @Operation(summary = "active profile 확인 API", description = "active profile을 확인하는 일종의 Health Check API입니다.")
     public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse(NULL);
+        return "UP";
     }
 
 }
