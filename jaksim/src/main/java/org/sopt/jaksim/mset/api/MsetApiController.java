042c65e37def261fdae4d38a665add202788d117
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-07-17 21:12:23 +0900

    Feat [#125] MsetApiController 수정

--- jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
+++ jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApiController.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.sopt.jaksim.category.api.CategoryApi;
 import org.sopt.jaksim.category.dto.CategoryMsetLinkResponse;
+import org.sopt.jaksim.category.dto.TaskMsetLinkResponse;
 import org.sopt.jaksim.category.facade.CategoryMsetFacade;
+import org.sopt.jaksim.category.facade.TaskMsetFacade;
 import org.sopt.jaksim.global.common.ApiResponseUtil;
 import org.sopt.jaksim.global.common.BaseResponse;
 import org.sopt.jaksim.global.message.SuccessMessage;
@@ -21,11 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class MsetApiController implements MsetApi {
     private final CategoryMsetFacade categoryMsetFacade;
+    private final TaskMsetFacade taskMsetFacade;
 
     @GetMapping("/mset/categories/{categoryId}")
     @Override
     public ResponseEntity<BaseResponse<?>> getFromOtherCategory(@PathVariable("categoryId") Long categoryId) {
         CategoryMsetLinkResponse response = categoryMsetFacade.getFromOtherCategory(categoryId);
+        return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
+    }
+
+    @GetMapping("/mset/tasks/{taskId}")
+    //@Override
+    public ResponseEntity<BaseResponse<?>> getFromOtherTask(@PathVariable("taskId") Long taskId) {
+        TaskMsetLinkResponse response = taskMsetFacade.getFromOtherTask(taskId);
         return ApiResponseUtil.success(SuccessMessage.SUCCESS, response);
     }
 }
