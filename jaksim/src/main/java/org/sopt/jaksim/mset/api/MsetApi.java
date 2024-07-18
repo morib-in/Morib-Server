52aa1cb76c56773ea73a14ca7227c71bcefe2651
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-07-18 22:40:39 +0900

    Fix [#143] 특정 태스크의 모립세트 불러오기 api

--- jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApi.java
+++ jaksim/src/main/java/org/sopt/jaksim/mset/api/MsetApi.java
@@ -8,8 +8,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.sopt.jaksim.category.dto.CategoryMsetLinkResponse;
 import org.sopt.jaksim.global.common.BaseResponse;
+import org.sopt.jaksim.mset.dto.MsetOfTask;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Tag(name = "Mset 관련 API")
 @SecurityRequirement(name = "Authorization")
@@ -46,5 +49,38 @@ public interface MsetApi {
                             responseCode = "500",
                             description = "서버 내부 오류입니다.")})
     ResponseEntity<BaseResponse<?>> getFromOtherCategory(@PathVariable("categoryId") Long categoryId);
+
+    @Operation(
+            summary = "특정 태스크의 모립세트 불러오기 API",
+            description = "특정 태스크의 모립세트를 불러오는 API입니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "요청이 성공했습니다.",
+                            content = @Content(schema = @Schema(implementation = MsetOfTask.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "잘못된 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰의 값이 올바르지 않습니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "대상을 찾을 수 없습니다",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "405",
+                            description = "잘못된 HTTP method 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류입니다.")})
+    ResponseEntity<BaseResponse<?>> getFromOtherTask(@PathVariable("taskId") Long taskId);
 
 }
